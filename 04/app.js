6c042fe9fbb59f8b2a251969a1cb908696e7e4ff
MarcinMorgan <morgangzit@gmail.com>
2024-06-06 21:45:27 +0200

    t4 button event listener added

--- 04/app.js
+++ 04/app.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', init);
 
+// https://api.weatherbit.io/v2.0/current?key=[key]&lat=[latitude]&lon=[longitude]
+
 function init() {
     console.log('DOM');
+
+    const weatherBtn = document.querySelector('.form__submit');
+    
+    weatherBtn.addEventListener('click', element => {
+        element.preventDefault();
+    })
+
 }
