46906502a423e5377f15b249edd5f4b8e4e38e30
MarcinMorgan <morgangzit@gmail.com>
2024-06-06 20:38:56 +0200

    t3 done, fetch user ip

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', init);
 
+const btn = document.querySelector('button');
+
 function init() {
     console.log('DOM');
+
+    btn.addEventListener('click', () => {
+        const promise = fetch('https://api.ipify.org?format=json');
+        
+        promise
+            .then(reqJSON => reqJSON.text())
+            .then(resp => { btn.nextElementSibling.innerText = JSON.parse(resp).ip})
+    })
 }
