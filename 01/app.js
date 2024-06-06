aebc0285cf653708640c6fd87c5358491dbc625b
MarcinMorgan <morgangzit@gmail.com>
2024-06-06 16:37:36 +0200

    t1 refactor

--- 01/app.js
+++ 01/app.js
@@ -3,21 +3,23 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     const divList = document.querySelectorAll('div');
     
-    setBorderColorAsync(divList, 0, 'red', firstCallback);
+    setBorderColorAsync(divList[0], 'red', firstCallback)
+
+
+
+    function firstCallback() {
+        setBorderColorAsync(divList[1], 'blue', secondCallback);
+    }
+
+    function secondCallback() {
+        setBorderColorAsync(divList[2], 'green', fn => {console.log('finish')});
+    }
 
 }
 
-function firstCallback(element) {
-    setBorderColorAsync(element, 1, 'blue', secondCallback);
-}
-
-function secondCallback(element) {
-    setBorderColorAsync(element, 2, 'green', () => {console.log('finish')});
-}
-
-function setBorderColorAsync(element, index, color, callback) {
+function setBorderColorAsync(element, color, callback) {
     setTimeout(() => {
-        element[index].style.border = `3px solid ${color}`;
+        element.style.border = `3px solid ${color}`;
         callback(element);
     }, Math.random() * 3000);
 }
