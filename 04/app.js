9184e4d8b752132d142e989060c79105d6e99c48
MarcinMorgan <morgangzit@gmail.com>
2024-06-12 20:02:17 +0200

    t4 done, request weather from API based on geographic coordinates

--- 04/app.js
+++ 04/app.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', init);
 
-// https://api.weatherbit.io/v2.0/current?key=[key]&lat=[latitude]&lon=[longitude]
-
 function init() {
     console.log('DOM');
 
@@ -9,6 +7,32 @@ function init() {
     
     weatherBtn.addEventListener('click', element => {
         element.preventDefault();
+        
+        const latInput = document.querySelector('.form__field--lat');
+        const lngInput = document.querySelector('.form__field--lng');
+        const latOutput = document.querySelector('.weather__lat');
+        const lngOutput = document.querySelector('.weather__lng')
+        const weatherSummary = document.querySelector('.weather__summary');
+        const weatherTemp = document.querySelector('.weather__temperature');
+
+        const lat = parseFloat(latInput.value);
+        const lng = parseFloat(lngInput.value);
+        const key = '';
+        if(!isNaN(lat) && !isNaN(lng)) {
+            latOutput.innerText = lat;
+            lngOutput.innerText = lng;
+        };
+
+        const promise = fetch(`https://api.weatherbit.io/v2.0/current?key=${key}&lat=${lat}&lon=${lng}&lang=pl&units=I`);
+
+        promise
+           .then(reqJSON => reqJSON.text())
+           .then(resp => { 
+                const dataJSON = JSON.parse(resp);
+                const arrJSON = {...dataJSON.data};
+                weatherTemp.innerText = arrJSON[0].app_temp;
+                weatherSummary.innerText = arrJSON[0].weather.description;
+            })
     })
 
-}
+} 
